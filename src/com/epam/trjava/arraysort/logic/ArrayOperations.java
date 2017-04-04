253d9553b21c46da0ed5f053011b6c4bd520e157
Mikhail_Bahatyrou <Mikhail_Bahatyrou@EPBYMINW2706.minsk.epam.com>
2017-04-04 11:46:23 +0300

    ArrayOperations comments added

--- src/com/epam/trjava/arraysort/logic/ArrayOperations.java
+++ src/com/epam/trjava/arraysort/logic/ArrayOperations.java
@@ -1,13 +1,25 @@
 package com.epam.trjava.arraysort.logic;
 
+/**
+ * Class for some operations of double[] array
+ * 
+ * @autor Mikhail Bahatyrou
+ * @version 1.0
+ */
+
 public final class ArrayOperations {
-	
-	private ArrayOperations(){}
-	
-	public static void printArray(double[] array){
-		for(double temp : array){
+
+	private ArrayOperations() {
+	}
+
+	/**
+	 * Method allows to print array of double[]
+	 * 
+	 * @param array - array to print
+	 */
+	public static void printArray(double[] array) {
+		for (double temp : array) {
 			System.out.print(temp + " ; ");
 		}
 	}
-
 }
